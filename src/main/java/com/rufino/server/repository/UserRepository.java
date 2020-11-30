14373bc732915f7cc7ab18c0b06356e22a36c8b0
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-30 16:31:50 -0300

    Saving user

---
+++ src/main/java/com/rufino/server/repository/UserRepository.java
@@ -0,0 +1,57 @@
+package com.rufino.server.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.rufino.server.dao.UserDao;
+import com.rufino.server.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository implements UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public User insertUser(UUID id, User user) throws Exception{
+        int result = jdbcTemplate.update(
+                "INSERT INTO users " + "(user_id, user_name, user_nickname, user_email, user_password, created_at)"
+                        + "VALUES (?, ?, ?, ?, ?, ?)",
+                id, user.getUserName(), user.getUserNickname(), user.getUserEmail(), user.getUserPassword(),
+                user.getCreatedAt());
+        user.setUserId((result > 0 ? id : null));
+        return user;
+    }
+
+    @Override
+    public int deleteUser(UUID id) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User getUser(UUID id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User updateUser(UUID id, User user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Autowired
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+}
