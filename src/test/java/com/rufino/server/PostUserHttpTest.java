c89d6d627951c4430b4fbc10497146bf530996f6
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-12-02 13:12:14 -0300

    Verifying jwt

--- src/test/java/com/rufino/server/PostUserHttpTest.java
+++ src/test/java/com/rufino/server/PostUserHttpTest.java
@@ -22,7 +22,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rufino.server.model.User;
 
@@ -68,7 +67,7 @@ public class PostUserHttpTest {
         assert (passwordEncoder.matches("123456", hashedPassword));
 
         JWTVerifier verifier = JWT.require(algorithm).build();
-        DecodedJWT jwt = verifier.verify(res.getString("token"));
+        verifier.verify(res.getString("token"));
     }
 
     @Test
